63bcc6a7182dbc04b4c20123506ccffe41343ff9
Matías Leporini <matilepo03@gmail.com>
2024-12-03 22:13:51 -0300

    Small change

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -241,6 +241,7 @@ const styles = StyleSheet.create({
   viewMoreText: {
     color: "#4B00B8",
     textDecorationLine: "underline",
+    marginBottom: 10,
   },
   backgroundIcon: {
     position: "absolute",
